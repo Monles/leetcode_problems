fa2407b223970d81fed6e67ff19f1c90a4198e14
monles <iceueb@gmail.com>
2022-06-14 04:09:25 +0100

    TOWSUM

--- 01-twoSum/twoSum.js
+++ 01-twoSum/twoSum.js
@@ -1,6 +1,18 @@
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
 function twoSum(nums, target) {
   const comp = new map();
   const len = nums.length;
 
-  for (let i = 0; i < len; i++) {}
+  for (let i = 0; i < len; i++) {
+    if (comp[nums[i]] >= 0) {
+      return [comp[nums[i]], i];
+    }
+    comp[target - nums[i]] = i;
+  }
+  return [];
 }
+const nums = [2, 7, 11, 15];
+console.log(twoSum(nums, 9));
