414c2fa8fb9f1655f74263f816340d3097e5e0f9
monles <iceueb@gmail.com>
2022-06-14 04:32:40 +0100

    add-two-numbers

---
+++ 02-Add-two-numbers/add-two-numbers.js
@@ -0,0 +1,28 @@
+// Input: l1 = [2,4,3], l2 = [5,6,4]
+// Output: [7,0,8]
+// Explanation: 342 + 465 = 807.
+
+var addTwoNumbers = function (l1, l2) {
+  let carry = 0;
+  let previousNode = new ListNode();
+  const headNode = previousNode;
+  while (l1 || l2 || carry) {
+    let val1 = 0;
+    let val2 = 0;
+    if (l1) {
+      val1 = l1.val;
+      l1 = l1.next;
+    }
+    if (l2) {
+      val2 = l2.val;
+      l2 = l2.next;
+    }
+    const sum = val1 + val2 + carry;
+    carry = Math.floor(sum / 10);
+    const digit = sum % 10;
+    const currentNode = new ListNode(digit);
+    previousNode.next = currentNode;
+    previousNode = currentNode;
+  }
+  return headNode.next;
+};
