d483f41f3ad518fc95b946847960175bc25132a6
monles <iceueb@gmail.com>
2022-06-14 07:02:53 +0100

    leetcode 04-06

--- 05-Longest-Palindromic-Substring/05-Longest-Palindromic-Substring.js
+++ 05-Longest-Palindromic-Substring/05-Longest-Palindromic-Substring.js
@@ -11,26 +11,24 @@
 // s consist of only digits and English letters.
 
 var longestPalindrome = function (s) {
-  if (s.length < 2) return s;
+  if (s.length < 3) return s;
   let max = "";
+
   for (let i = 0; i < s.length; i++) {
     let left = helper(i, i, s);
     let right = helper(i, i + 1, s);
-    let currMax = left.length >= right.length ? left : right;
-    max = currMax.length > max.length ? currMax : max;
+    let currMax = left.lenght >= right.length ? left : right;
+    max = currMax.length >= max.length ? currMax : max;
   }
-
   return max;
 };
 
 function helper(left, right, s) {
   let curr = "";
-
   while (left >= 0 && right < s.length && s[left] === s[right]) {
     curr = s.substring(left, right + 1);
     left -= 1;
     right += 1;
   }
-
   return curr;
 }
