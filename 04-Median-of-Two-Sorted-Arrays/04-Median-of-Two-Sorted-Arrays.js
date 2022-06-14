8f8e0a43d1930d812b7eb18d37e955e2baa423df
monles <iceueb@gmail.com>
2022-06-14 06:26:25 +0100

    leetcode04-05

--- 04-Median-of-Two-Sorted-Arrays/04-Median-of-Two-Sorted-Arrays.js
+++ 04-Median-of-Two-Sorted-Arrays/04-Median-of-Two-Sorted-Arrays.js
@@ -3,18 +3,15 @@
 // The overall run time complexity should be O(log (m+n)).
 
 // Example 1:
-
 // Input: nums1 = [1,3], nums2 = [2]
 // Output: 2.00000
 // Explanation: merged array = [1,2,3] and median is 2.
 // Example 2:
-
 // Input: nums1 = [1,2], nums2 = [3,4]
 // Output: 2.50000
 // Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
 
 // Constraints:
-
 // nums1.length == m
 // nums2.length == n
 // 0 <= m <= 1000
@@ -31,12 +28,11 @@ function findMedianSortedArrays(nums1 = [], nums2 = []) {
   const len2 = nums2.length;
   const len = len1 + len2;
 
-  if (len === 0) {
+  if (len == 0) {
     return null;
   }
 
   const merged = [];
-
   while (i1 < len1 && i2 < len2) {
     if (nums1[i1] <= nums2[i2]) {
       merged.push(nums1[i1++]);
@@ -48,6 +44,7 @@ function findMedianSortedArrays(nums1 = [], nums2 = []) {
   while (i1 < len1) {
     merged.push(nums1[i1++]);
   }
+
   while (i2 < len2) {
     merged.push(nums2[i2++]);
   }
@@ -56,7 +53,7 @@ function findMedianSortedArrays(nums1 = [], nums2 = []) {
   if (isOdd) {
     return merged[(len - 1) / 2];
   } else {
-    return (merged[merged.length / 2] + merged[merged.length / 2 - 1]) / 2;
+    return (merged[len / 2] + merged[len / 2 - 1]) / 2;
   }
 }
 
---
+++ 05-Longest-Palindromic-Substring/05-Longest-Palindromic-Substring.js
@@ -0,0 +1,36 @@
+// Example 1:
+// Input: s = "babad"
+// Output: "bab"
+// Explanation: "aba" is also a valid answer.
+// Example 2:
+// Input: s = "cbbd"
+// Output: "bb"
+
+// Constraints:
+// 1 <= s.length <= 1000
+// s consist of only digits and English letters.
+
+var longestPalindrome = function (s) {
+  if (s.length < 2) return s;
+  let max = "";
+  for (let i = 0; i < s.length; i++) {
+    let left = helper(i, i, s);
+    let right = helper(i, i + 1, s);
+    let currMax = left.length >= right.length ? left : right;
+    max = currMax.length > max.length ? currMax : max;
+  }
+
+  return max;
+};
+
+function helper(left, right, s) {
+  let curr = "";
+
+  while (left >= 0 && right < s.length && s[left] === s[right]) {
+    curr = s.substring(left, right + 1);
+    left -= 1;
+    right += 1;
+  }
+
+  return curr;
+}
