7de13f19e4003bf8a6315df16ab743a113683bb5
monles <iceueb@gmail.com>
2022-06-19 06:52:25 +0100

    21

--- 21-Merge-Two-Sorted-Lists/21-Merge-Two-Sorted-Lists.js
+++ 21-Merge-Two-Sorted-Lists/21-Merge-Two-Sorted-Lists.js
@@ -3,8 +3,6 @@ var mergeTwoLists = function (l1, l2) {
   let head = list;
 
   while (l1 !== null && l2 !== null) {
-    // Select the smallest value from either linked list,
-    // then increment that list forward.
     if (l1.val < l2.val) {
       list.next = new ListNode(l1.val);
       l1 = l1.next;
@@ -16,11 +14,8 @@ var mergeTwoLists = function (l1, l2) {
     list = list.next;
   }
 
-  // It's possible that one linked list is shorter than the other so we just
-  // add on the remainder of the last linked list. It's already sorted :)
   if (l1 !== null) list.next = l1;
   if (l2 !== null) list.next = l2;
 
-  // return .next because this first element in the linkedlist is empty
   return head.next;
 };
