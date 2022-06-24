3a93dac789072aab39cb343f9a43ad3738222df5
monles <iceueb@gmail.com>
2022-06-24 06:55:53 +0100

    day34-36

---
+++ 36-Valid-Sudoku/36-Valid-Sudoku.js
@@ -0,0 +1,22 @@
+var isValidSudoku = function (board) {
+  let seen = new Set();
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      let cell = board[i][j];
+      if (cell != '.') {
+        let colCell = `cell: ${j} ${cell}`;
+        let rowCell = `row: ${i} ${cell}`;
+        let subBoxCell = `subBox: ${parseInt(i / 3)}-${parseInt(
+          j / 3
+        )} ${cell}`;
+
+        if (!seen.has(colCell) && !seen.has(rowCell) && !seen.has(subBoxCell)) {
+          seen.add(colCell);
+          seen.add(rowCell);
+          seen.add(subBoxCell);
+        } else return false;
+      }
+    }
+  }
+  return true;
+};
